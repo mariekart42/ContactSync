0b9a8219041610742bea9e01b1d7de0a5b97a714
mariekart42 <marie.a.mensing@gmail.com>
2025-08-27 13:58:16 +0200

    finished logic to create folders and change contacts within + test. Next: more tests and cleanup

--- src/main/java/org/example/OutlookContactUpdater.java
+++ src/main/java/org/example/OutlookContactUpdater.java
@@ -4,11 +4,10 @@ import com.microsoft.graph.models.Contact;
 import com.microsoft.graph.models.ContactFolder;
 import com.microsoft.graph.models.ContactFolderCollectionResponse;
 import com.microsoft.graph.users.item.UserItemRequestBuilder;
-import com.microsoft.graph.users.item.contactfolders.ContactFoldersRequestBuilder;
 import com.microsoft.graph.users.item.contacts.ContactsRequestBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.example.AditoRequests.CONTACT_STATUS.TO_CREATE;
 import static org.example.AditoRequests.CONTACT_STATUS.TO_DELETE;
@@ -16,17 +15,18 @@ import static org.example.AditoRequests.CONTACT_STATUS.TO_DELETE;
 
 public class OutlookContactUpdater {
 
-    ContactFolderCollectionResponse _contactFolders;
-
     // TODO:
     //  - catch syncresult, store it in `SYNCABONNEMENT.syncresult`
     //  - update `SYNCABONNEMENT.synced`, `SYNCABONNEMENT.changed`, `SYNCABONNEMENT.abostart` and `SYNCABONNEMENT.aboende`
     //  - create folder and put contact inside with information of 'device' -> later: move contact to new folder if it changed
-    public static void updateContact(Contact contact, String user, String contactLuid, AditoRequests.CONTACT_STATUS status)
+    public static void updateContact(Contact contact, Map<String, String> contactMetaData, String user, AditoRequests.CONTACT_STATUS status)
     {
+        // TODO ask about 'user'. should i retrieve user from device? if yes, user here is redundant
+        String luid = contactMetaData.get("luid");
+        String device = contactMetaData.get("device");
+
         if (contact == null)
             return;
-
 
         AppConfig config = new AppConfig();
         try
@@ -40,227 +40,87 @@ public class OutlookContactUpdater {
                     .users()
                     .byUserId(user);
 
-            boolean categoryUpdate = addAditoCategoryToContact(contacts, contact, contactLuid, status);
-            moveContactToCorrectFolder(graphClient, contact, contactLuid);
+            boolean categoryUpdate = addAditoCategoryToContact(contacts, contact, luid, status);
 
-//            switch (status)
-//            {
-//                case TO_CHANGE -> {
-//                    System.out.println("\033[0;33mCONTACT:\n\tID: " + contactLuid + "\n\tNAME: " + contact.getDisplayName() + "\nhas status TO_CHANGE -> PATCH to Outlook\033[0m");
-//                    contacts.byContactId(contactLuid).patch(contact);
-//                }
-//                case TO_CREATE -> {
-//                    contacts.post(contact);
-//                    System.out.println("\033[0;32mCONTACT:\n\tID: " + contactLuid + "\n\tNAME: " + contact.getDisplayName() + "\nhas status TO_CREATE -> POST to Outlook\033[0m");
-//                }
-//                case TO_DELETE -> {
-//                    //contacts.byContactId(contactID).delete();
-//                    System.out.println("\033[0;31mCONTACT:\n\tID: " + contactLuid + "\n\tNAME: " + contact.getDisplayName() + "\nhas status TO_DELETE -> DELETE to Outlook\033[0m");
+
+
+//            moveContactToCorrectFolder(graphClient, contact, device);
+            String deepestId = ensureFolderExists(graphClient, null, extractFolderTree(device));
+
+            // question: van i update any user without knowing his folder or do i need to figure out where he is in order to change him
+            // answer: no i do not need the folder id
+
+            switch (status)
+            {
+                case TO_CHANGE -> {
+                    contacts.byContactId(luid).patch(contact);
+                    System.out.println("\033[0;33mCONTACT:\n\tID: " + luid + "\n\tNAME: " + contact.getDisplayName() + "\nhas status TO_CHANGE -> PATCH to Outlook\033[0m");
+                }
+                case TO_CREATE -> {
+                    Contact lol;
+                    if (deepestId == null || deepestId.isEmpty())// happens if device has no folder specified, eg device: mensing@kieback-peter.de/
+                        lol = graphClient.contacts().post(contact);
+                    else
+                        lol = graphClient.contactFolders().byContactFolderId(deepestId).contacts().post(contact);
+
+                    System.out.println("\033[0;32mCONTACT:\n\tID: " + lol.getId() + "\n\tNAME: " + lol.getDisplayName() + "\nhas status TO_CREATE -> POST to Outlook\033[0m");
+                }
+                case TO_DELETE -> {
+                    contacts.byContactId(luid).delete();
+                    System.out.println("\033[0;31mCONTACT:\n\tID: " + luid + "\n\tNAME: " + contact.getDisplayName() + "\nhas status TO_DELETE -> DELETE to Outlook\033[0m");
 //                    System.out.println("We skip deleting for now, easier to test program rn");// TODO: delete later, uncomment prev line
-//                }
-//                case UNCHANGED -> {
-//                    if (categoryUpdate)
-//                        contacts.byContactId(contactLuid).patch(contact);
-//                    System.out.println("\033[0;36mCONTACT:\n\tID: " + contactLuid + "\n\tNAME: " + contact.getDisplayName() + "\nhas status UNCHANGED -> nothing to Outlook\033[0m");
-//                }
-//                default -> throw new Exception("CONTACT_STATUS has unexpected value: " + status);
-//            }
+                }
+                case UNCHANGED -> {
+                    if (categoryUpdate)
+                        contacts.byContactId(luid).patch(contact);
+                    System.out.println("\033[0;36mCONTACT:\n\tID: " + luid + "\n\tNAME: " + contact.getDisplayName() + "\nhas status UNCHANGED -> nothing to Outlook\033[0m");
+                }
+                default -> throw new Exception("CONTACT_STATUS has unexpected value: " + status);
+            }
         }
         catch (Exception e)
         {
             System.out.println("An error occurred while updating a contact in Outlook: " + e);
-            System.out.println("Debug stack Trace:");
             e.printStackTrace();
         }
     }
 
-    private static void moveContactToCorrectFolder(UserItemRequestBuilder graphClient, Contact contactData, String contactLuid) {
-        System.out.println();
-//        var contactsFolders = graphClient.contactFolders().get();
-        System.out.println();
+    // Creates the folder tree for a contact based on the device variable
+    // Handles cases where all, some, or none of the folders already exist
 
-        // create folder, best another folder inside
-
-             // get parent folder id
-//        var contactInstance = graphClient.contacts().byContactId(contactLuid).get();
-//        assert contactInstance != null;
-//        var parentFolderId = contactInstance.getParentFolderId();
-
-
-
-
-
-        /**
-         * {
-         *   "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#users('mensing%40kieback-peter.de')/contactFolders",
-         *   "value": [
-         *     {
-         *       "id": "AAMkADg4NWM4MjMxLTc0ZjQtNGU4ZC05ZDc2LTgwMDJkNjAyNWE0YwAuAAAAAABb4L58lBuvQaM7Snxz45CZAQDKuT-HM9gZQIxqxJFFZgGEAABulsmlAAA=",
-         *       "parentFolderId": "AAMkADg4NWM4MjMxLTc0ZjQtNGU4ZC05ZDc2LTgwMDJkNjAyNWE0YwAuAAAAAABb4L58lBuvQaM7Snxz45CZAQDKuT-HM9gZQIxqxJFFZgGEAAAAAAEOAAA=",
-         *       "displayName": "silly contacts"
-         *     },
-         *   ]
-         * }
-         *
-         * AAMkADg4NWM4MjMxLTc0ZjQtNGU4ZC05ZDc2LTgwMDJkNjAyNWE0YwAuAAAAAABb4L58lBuvQaM7Snxz45CZAQDKuT-HM9gZQIxqxJFFZgGEAAAAAAEOAAA=
-         */
-
-
-
-
-
-
-
-
-
-        ContactFolder contactFolder = new ContactFolder();
-        contactFolder.setDisplayName("AditoKontakte");
-        var motherFolder = graphClient.contactFolders().post(contactFolder);
-
-        // create child folder inside
-        ContactFolder childFolderData = new ContactFolder();
-        childFolderData.setDisplayName("child1");
-        var childFolder = graphClient.contactFolders().byContactFolderId(motherFolder.getId()).childFolders().post(childFolderData);
-
-        // create another child folder in child folder
-        ContactFolder nestedChildFolderData = new ContactFolder();
-        nestedChildFolderData.setDisplayName("child2");
-        var nestedChildFolder = graphClient.contactFolders().byContactFolderId(childFolder.getId()).childFolders().post(nestedChildFolderData);
-
-
-
-//        // create contact there and try to get him
-//        Contact motherContact = new Contact();
-//        motherContact.setDisplayName("Mother3");
+    // TODO create logic for not just creating the user in that folder but to act based on the status
+//    private static void moveContactToCorrectFolder(UserItemRequestBuilder graphClient, Contact contactData, String device) throws Exception {
 //
-//        Contact childContact = new Contact();
-//        childContact.setDisplayName("Child3");
+//        String deepestId = ensureFolderExists(graphClient, null, extractFolderTree(device));
 //
-//        // create contact in mother folder:
-//        graphClient.contactFolders().byContactFolderId(motherFolder.getId()).contacts().post(motherContact);
+////      contactData.setDisplayName("hihi");
 //
-//        // create contact in child folder:
-//        var childInfo = graphClient.contactFolders().byContactFolderId(childFolder.getId()).contacts().post(childContact);
-//
-//
-//        var tmp = graphClient.contacts().byContactId(childInfo.getId()).get();
-//
-//
-//        var allContactsFolders = graphClient.contactFolders().get();
-
-
-
-        // device:
-        // mensing@kieback-peter.de/
-        // mensing@kieback-peter.de/AditoKontakt
-        // mensing@kieback-peter.de/AditoKontakt/lol
-
-        String deviceFolder1 = "AditoKontakte";
-//        String deviceFolder2 = "lol";
-//        String deviceFolder3 = "lol2";
-
-
-        // can we from there see in what folder he is? cause later we want to figure out if hes in the right folder or if we have to move him
-        // get first layer folders
-        var layer1folder = graphClient.contactFolders().get();
-//        var tmp3 = layer1folder.getValue();
-
-
-        String device = "mensing@kieback-peter.de/AditoKontakte/child1/child2/child3/";
-        boolean folderExists = false;
-        String parentFolderId = "";
-        for (var folder1 : layer1folder.getValue())
-        {
-            if (folder1.getDisplayName().equalsIgnoreCase(deviceFolder1))
-            {
-                folderExists = true;
-                parentFolderId = folder1.getId();
-                break;
-            }
-        }
-
-        if (folderExists)
-        {
-            device = "nope/nope/child3/";
-            assert parentFolderId != null;
-            String deepestId = ensureFolderExists(graphClient, parentFolderId, device);
-            System.out.println();
-        }
-        else
-        {
-            device = "AditoKontakte/child1/child2/child3/";
-            // logic to create AditoKontakte folder and all others
-            // create mother folder
-            // give id to next function
-            String motherFolderId = "";
-            createFolders(graphClient, motherFolderId, device);
-        }
-
-        //AAMkADg4NWM4MjMxLTc0ZjQtNGU4ZC05ZDc2LTgwMDJkNjAyNWE0YwAuAAAAAABb4L58lBuvQaM7Snxz45CZAQDKuT-HM9gZQIxqxJFFZgGEAACNcn22AAA=
-        //AAMkADg4NWM4MjMxLTc0ZjQtNGU4ZC05ZDc2LTgwMDJkNjAyNWE0YwAuAAAAAABb4L58lBuvQaM7Snxz45CZAQDKuT-HM9gZQIxqxJFFZgGEAACNcn22AAA=
-
-
-//
-//
-//
-//        if (folderExists)
-//        {
-//            folderExists = false;
-//            var layer2folder = graphClient.contactFolders().byContactFolderId(parentFolderId).childFolders().get();
-//
-//
-//            for (var folder2 : layer2folder.getValue())
-//            {
-//                if (folder2.getDisplayName().equalsIgnoreCase(deviceFolder2))
-//                {
-//                    folderExists = true;
-//                    parentFolderId = folder2.getId();
-//                }
-//            }
-//
-//            if (folderExists)
-//            {
-//                folderExists = false;
-//
-//                var layer3folder = graphClient.contactFolders().byContactFolderId(parentFolderId).childFolders().get();
-//                for (var folder3 : layer3folder.getValue())
-//                {
-//                    if (folder3.getDisplayName().equalsIgnoreCase(deviceFolder3))
-//                    {
-//                        folderExists = true;
-//                        parentFolderId = folder3.getId();
-//                    }
-//                }
-//
-//                if (folderExists)
-//                {
-//                    // same as before
-//                }
-//                else {
-//                    // logic to create all folder after second one
-//                }
-//            }
-//            else {
-//                // logic to create all folder after first one
-//            }
-//        }
+//        if (deepestId == null || deepestId.isEmpty())// happens if device has no folder specified, eg device: mensing@kieback-peter.de/
+//            graphClient.contacts().post(contactData);
 //        else
-//        {
-//            // logic to create all folder
-//        }
+//            graphClient.contactFolders().byContactFolderId(deepestId).contacts().post(contactData);
+//    }
 
 
+    // Removes the user prefix from the device string and returns only the folder path
+    private static String extractFolderTree(String device)
+    {
+        if (device == null || device.isEmpty())
+            return null;
 
+        int slashIndex = device.indexOf('/');
+        if (slashIndex == -1)
+            return null; // no folders present
 
-        System.out.println();
+        return device.substring(slashIndex + 1);
     }
 
-    private static String ensureFolderExists(UserItemRequestBuilder graphClient, String parentFolderId, String device) {
-        if (device.isEmpty())
-            return parentFolderId;
 
-        String newParentFolderId = "";
-        boolean folderExists = false;
+    // function recursively traverses through the folder tree based on 'device'
+    // Creates missing folders if they do not exist
+    private static String ensureFolderExists(UserItemRequestBuilder graphClient, String parentFolderId, String device) throws Exception {
+        if (device == null || device.isEmpty() || device.equalsIgnoreCase("/"))
+            return parentFolderId;
 
         String folderName;
         device = trimChar(device, '/');
@@ -273,7 +133,7 @@ public class OutlookContactUpdater {
                 device = "";
             }
             else
-                return ensureFolderExists(graphClient, null, "");
+                return parentFolderId;
         }
         else
         {
@@ -281,34 +141,70 @@ public class OutlookContactUpdater {
             device = device.substring(nextSlash + 1);
         }
 
-        var folderCollection = graphClient.contactFolders().byContactFolderId(parentFolderId).childFolders().get();
+        String folderId = findFolderId(graphClient, parentFolderId, folderName);
+        if (folderId == null)
+            return createFolders(graphClient, parentFolderId, folderName + "/" + device);// no folder with specified name found, so we create it
+        else
+            return ensureFolderExists(graphClient, folderId, device);
+    }
 
-        for (var folder : folderCollection.getValue())
+
+    private static String findFolderId(UserItemRequestBuilder graphClient, String parentFolderId, String folderName) {
+        // if parentFolderId is null or invalid/does not exist, function will throw error, caught by updateContact catch-block
+        // TODO maybe catch this error message and put it in the syncupdate
+        ContactFolderCollectionResponse folderCollection;
+
+        if (parentFolderId == null || parentFolderId.isEmpty())
+            folderCollection = graphClient.contactFolders().get();
+        else
+            folderCollection = graphClient.contactFolders().byContactFolderId(parentFolderId).childFolders().get();
+
+        for (var folder : folderCollection.getValue()) // no null/empty check needed, see comment above
         {
             if (folder.getDisplayName().equalsIgnoreCase(folderName))
-            {
-                folderExists = true;
-                newParentFolderId = folder.getId();
-            }
+                return folder.getId();
         }
+        return null;
+    }
 
-        if (folderExists)
-        {
-            return ensureFolderExists(graphClient, newParentFolderId, device);
-        }
-        else
-        {
-            createFolders(graphClient, parentFolderId, device);
+
+    protected static String createFolders(UserItemRequestBuilder graphClient, String parentFolderId, String device) throws Exception {
+        if (device == null || device.isEmpty() || device.equalsIgnoreCase("/"))
             return parentFolderId;
+
+        device = trimChar(device, '/');
+        String[] folderArray = device.split("/");
+        if (folderArray.length < 1)
+            return parentFolderId;
+
+        for (String folder : folderArray)
+        {
+            if (folder.isEmpty())
+                continue;
+
+            ContactFolder newFolder = new ContactFolder();
+            newFolder.setDisplayName(folder);
+            ContactFolder newFolderResponse;
+            if (parentFolderId == null || parentFolderId.isEmpty())
+            {
+                try {
+                    newFolderResponse = graphClient.contactFolders().post(newFolder);
+                } catch (Exception e) {
+                    throw new Exception("Failed to create new Folder: " + e); // TODO catch this error message with stack trace later and put in syncresult
+                }
+            }
+            else
+                newFolderResponse = graphClient.contactFolders().byContactFolderId(parentFolderId).childFolders().post(newFolder);
+
+            parentFolderId = newFolderResponse.getId();
         }
+        return parentFolderId;
     }
 
-    private static void createFolders(UserItemRequestBuilder graphClient, String parentFolderId, String device) {
-        // TODO logic to create all missing folders
-        // loop through device till empty
-
+    protected static void deleteFolder(UserItemRequestBuilder graphClient, String folderId)
+    {
+        graphClient.contactFolders().byContactFolderId(folderId).delete();
     }
-
 
     private static String trimChar(String str, Character toRemove)
     {
---
+++ src/test/java/org/example/OutlookContactUpdaterTest.java
@@ -0,0 +1,157 @@
+package org.example;
+
+import com.microsoft.graph.models.Contact;
+import com.microsoft.graph.users.item.UserItemRequestBuilder;
+import org.example.AditoRequests.CONTACT_STATUS;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OutlookContactUpdaterTest {
+
+    @Test
+    void updateContact_createNewContactInMainFolder() throws Exception {
+        Contact contact = new Contact();
+        String testName = "oweihfpqieh837918yogiYR^Reyo2igifqgo";
+        contact.setDisplayName(testName);
+
+        Map<String, String> contactMetaData = new HashMap<>();
+        contactMetaData.put("luid", null);
+        contactMetaData.put("device", "mensing@kieback-peter.de/");
+
+        OutlookContactUpdater.updateContact(contact, contactMetaData, App.USER, CONTACT_STATUS.TO_CREATE);
+
+        AppConfig config = new AppConfig();
+        var graphClient = config.getGraphServiceClient().users().byUserId(App.USER);
+        var response = graphClient.contacts().get();
+        for (var contactItem : response.getValue())
+        {
+            if (contactItem.getDisplayName().equalsIgnoreCase(testName))
+            {
+                assertTrue(true);
+                return;
+            }
+        }
+        fail();
+    }
+
+
+    @Test
+    void updateContact_changeContactInMainFolder() throws Exception {
+        Contact contact = new Contact();
+        String newName = "owwwwwwwwwwwewfweqfqefo";
+        contact.setDisplayName(newName);
+
+        Map<String, String> contactMetaData = new HashMap<>();
+        contactMetaData.put("luid", "AAMkADg4NWM4MjMxLTc0ZjQtNGU4ZC05ZDc2LTgwMDJkNjAyNWE0YwBGAAAAAABb4L58lBuvQaM7Snxz45CZBwDKuT-HM9gZQIxqxJFFZgGEAAAAAAEOAADKuT-HM9gZQIxqxJFFZgGEAACPGe0-AAA=");
+
+        OutlookContactUpdater.updateContact(contact, contactMetaData, App.USER, CONTACT_STATUS.TO_CHANGE);
+
+        AppConfig config = new AppConfig();
+        var graphClient = config.getGraphServiceClient().users().byUserId(App.USER);
+        var response = graphClient.contacts().get();
+        for (var contactItem : response.getValue())
+        {
+            if (contactItem.getDisplayName().equalsIgnoreCase(newName))
+            {
+                assertTrue(true);
+                return;
+            }
+        }
+        fail();
+    }
+
+
+
+
+    @Test
+    void createFolders_createOneNewFolder()
+    {
+        try
+        {
+            var graphClient = getUserGraphClient();
+            String parentFolderId = null;
+            String device = "/doesNotExist/";
+            OutlookContactUpdater.createFolders(graphClient, parentFolderId, device);
+
+            var response = graphClient.contactFolders().get();
+            for (var folderItem : response.getValue())
+            {
+                if (folderItem.getDisplayName().equalsIgnoreCase("doesNotExist"))
+                {
+                    assertTrue(true);
+                    OutlookContactUpdater.deleteFolder(graphClient, folderItem.getId());
+                    return;
+                }
+            }
+            fail();
+
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void createFolders_createTwoNewNestedFolders()
+    {
+        System.out.println("MAKE SURE folder /doesNotExist/doesAlsoNotExist/ does not exist.");
+        try
+        {
+            var graphClient = getUserGraphClient();
+            String parentFolderId = null;
+            String device = "/doesNotExist/doesAlsoNotExist/";
+            try
+            {
+                OutlookContactUpdater.createFolders(graphClient, parentFolderId, device);
+            }
+            catch (Exception e)
+            {
+                fail(e);
+                return;
+            }
+
+            var response = graphClient.contactFolders().get();
+            for (var folderItem : response.getValue())
+            {
+                if (folderItem.getDisplayName().equalsIgnoreCase("doesNotExist"))
+                {
+                    response = graphClient.contactFolders().byContactFolderId(folderItem.getId()).childFolders().get();
+
+                    for (var folderItem2 : response.getValue())
+                    {
+                        if (folderItem2.getDisplayName().equalsIgnoreCase("doesAlsoNotExist"))
+                        {
+                            assertTrue(true);
+                            return;
+                        }
+                    }
+                    return;
+                }
+            }
+            fail();
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+
+
+    UserItemRequestBuilder getUserGraphClient() throws Exception
+    {
+        AppConfig config = new AppConfig();
+        try {
+            return config.getGraphServiceClient().users().byUserId(App.USER);
+        } catch (Exception e) {
+            throw new Exception("Unable to retrieve Graph Credentials: " + e);
+        }
+    }
+}
